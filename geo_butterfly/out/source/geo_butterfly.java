62002656bba8b1d9f9514229e88d52364c8cd92d
Ana Carolina Assis Jesus <anacarolina.assisjesus@gmail.com>
2019-11-13 11:52:54 +0100

    butterfly variations

--- geo_butterfly/out/source/geo_butterfly.java
+++ geo_butterfly/out/source/geo_butterfly.java
@@ -14,10 +14,11 @@ import java.io.IOException;
 
 public class geo_butterfly extends PApplet {
 
+int r = 0;
+
 public void setup() {
     
-    
-    background(0);
+    background(0xff91F085);
 
     frameRate(15);
 
@@ -25,58 +26,57 @@ public void setup() {
 
 public void draw() {
 
-    // fill(0,10);
-    // noStroke();
-    // rect(0, 0, width, height);
+    // // fill(random(255),0,random(255),50);
+    // fill(0,50);
+    fill(0xff91F085, 10);
+    noStroke();
+    rect(0, 0, width, height);
+    // background(#F0EB85);
 
     int n = PApplet.parseInt(random(3));
 
     if(n==0){
         noFill();
         stroke(255);    
+        // r = 0;
     }
     if(n==1){
-        fill(random(255),0,random(255));
+        fill(random(255),0,random(255),50);
         noStroke();
+        rotate(random(100));
+        // r = 0;
     }
     if(n==2){
-        fill(0,random(255),random(255));
+        fill(0,random(255),random(255),50);
         noStroke();    
+        rotate(0);
+        r = r + 3;
+        // r = int(random(-200,200));
     }
+    println("r: "+r);
+    if(r==300){r = -300;}
+    // Right Wings
+    triangle(400+r, 200+r, 450+r, 200+r, 425+r, 300+r);
+    triangle(400+r, 200+r, 425+r, 300+r, 350+r, 235+r);
+    triangle(350+r, 235+r, 425+r, 300+r, 330+r, 275+r);
 
-    triangle(400, 200, 450, 200, 425, 300);
-    triangle(400, 200, 425, 300, 350, 235);
-    triangle(350, 235, 425, 300, 330, 275);
+    triangle(330+r, 275+r, 415+r, 330+r, 340+r, 340+r);
+    triangle(415+r, 330+r, 340+r, 340+r, 375+r, 400+r);
 
-    triangle(330, 275, 415, 330, 340, 340);
-    triangle(415, 330, 340, 340, 375, 400);
+    // Left Wings
+    triangle(290+r, 235+r, 205+r, 300+r, 310+r, 275+r);
+    triangle(290+r, 235+r, 205+r, 300+r, 240+r, 200+r);
+    triangle(240+r, 200+r, 205+r, 300+r, 190+r, 200+r);
+
+    triangle(310+r, 275+r, 225+r, 330+r, 300+r, 340+r);
+    triangle(225+r, 330+r, 300+r, 340+r, 260+r, 400+r);
 
 
+    // Body
     noStroke();
     fill(255);
-    triangle(330, 275, 310, 275, 320, 245);
-    triangle(330, 275, 310, 275, 320, 320);
-
-
-    if(n==0){
-        noFill();
-        stroke(255);    
-    }
-    if(n==1){
-        fill(random(255),0,random(255));
-        noStroke();  
-    }
-    if(n==2){
-        fill(0,random(255),random(255));
-        noStroke();    
-    }
-    
-    triangle(290, 235, 205, 300, 310, 275);
-    triangle(290, 235, 205, 300, 240, 200);
-    triangle(240, 200, 205, 300, 190, 200);
-
-    triangle(310, 275, 225, 330, 300, 340);
-    triangle(225, 330, 300, 340, 260, 400);
+    triangle(330+r, 275+r, 310+r, 275+r, 320+r, 245+r);
+    triangle(330+r, 275+r, 310+r, 275+r, 320+r, 320+r);
 
     
 
