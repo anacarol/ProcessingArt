65ff3b121cf216295791075563814932cb2c074a
Ana Carolina Assis Jesus <anacarolina.assisjesus@gmail.com>
2020-01-04 00:21:08 +0100

    Beautiful Mandalas

--- 2020/genMandalas/build-tmp/source/genMandalas.java
+++ 2020/genMandalas/build-tmp/source/genMandalas.java
@@ -14,58 +14,117 @@ import java.io.IOException;
 
 public class genMandalas extends PApplet {
 
-// float r = 200;
-
-
 public void setup() {
 	
 	
-	background(255);
+	background(0);
 
 	noFill();
-	stroke(0, 40);
+	stroke(random(255),0,random(255), 40);
 
 	// frameRate(10);
 }
 
-public void draw() {
+
+
+public void draw(){
 
 	float t = (float)frameCount; // converting the count to a float
-
-	// // moving my origin canvas position 
-	// translate(0, height/2);   // move the coordinate to left middle
-
-	// fill(0,random(0,255),random(0,255));
-	// ellipse(t, sin(t/20)*100, 20,20);
-	// ellipse(t, -sin(t/20)*100, 20,20);
-
-	// // fill(random(0,255),0,random(0,255));
-	// // ellipse(t, cos(t/20)*100, 20,20);
-
-
-	// // // first experiment
-	// // ellipse(frameCount, sin(radians(frameCount))*200, 50, 50);
-	// // ellipse(frameCount, -sin(radians(frameCount))*200, 50, 50);
-
-
+	
 	// Moving my canvas origin to the center.
 	translate(width/2, height/2);
 
-	// ellipse(cos(t/20)*100, sin(t/20)*100, 20,20);
-	// ellipse(sin(t/20)*100, cos(t/20)*100, 20,20);
-
-	ellipse(sin(t/100)*100, cos(t/100)*100, 50,50);
- 
-
- // 	float circle_size = random(50);
- // 	fill(random(0,255),random(0,255),random(0,255));
- // 	ellipse(t + r, sin(t/20)*100, circle_size, circle_size);
- // 	ellipse(t + r, -sin(t/20)*100, circle_size, circle_size);
-
- // 	r = r - 0.2;
-
 	
+	// circle
+	ellipse(sin(t/40)*40, cos(t/40)*40, 30,30);
+	ellipse(sin(t/40)*100, cos(t/40)*100, 30,30);
+	ellipse(sin(t/40)*200, cos(t/40)*200, 30,30);
+	ellipse(sin(t/40)*300, cos(t/40)*300, 30,30);
+
+	rotate(t/20);
+
+	// ellipse
+	ellipse(sin(t/40)*40, cos(t/40)*100, 30,30);
+	ellipse(sin(t/40)*100, cos(t/40)*40, 30,30);
+
+	ellipse(sin(t), cos(t), 30,30);
+
+	ellipse(sin(t/40)*100, cos(t/40)*400, 30,30);
+	ellipse(sin(t/40)*400, cos(t/40)*100, 30,30);
+
+	ellipse(sin(t/40)*200, cos(t/40)*100, 30,30);
+	ellipse(sin(t/40)*100, cos(t/40)*200, 30,30);
+
+
 }
+
+
+
+// // rotating a triangle moving on a sin cos wave...
+// void draw(){
+
+// 	float t = (float)frameCount; // converting the count to a float
+	
+// 	// Moving my canvas origin to the center.
+// 	translate(width/2, height/2);
+
+// 	stroke(random(255),0,random(255), 40);
+
+// 	rotate(t/20);
+
+// 	triangle(sin(t/40)*100, t, cos(t/40)*100, t, 30, 30);
+// 	triangle(-sin(t/40)*100, t, -cos(t/40)*100, t, 30, 30);
+
+// }
+
+
+
+// // making a perfect circle, using cos and sin for x and y, and centering in the middle of the canvas
+// void draw() {
+
+// 	float t = (float)frameCount; // converting the count to a float
+	
+// 	// Moving my canvas origin to the center.
+// 	translate(width/2, height/2);
+
+// 	ellipse(cos(t/20)*100, sin(t/20)*100, 20,20);
+// 	ellipse(sin(t/20)*100, cos(t/20)*100, 20,20);
+
+// 	ellipse(sin(t/100)*100, cos(t/100)*100, 50,50);
+	
+// }
+
+
+
+// // second experiment: transform the count into float
+// void draw(){
+
+// 	float t = (float)frameCount; // converting the count to a float
+
+// 	// moving my origin canvas position 
+// 	translate(0, height/2);   // move the coordinate to left middle
+
+// 	fill(0,random(0,255),random(0,255));
+// 	ellipse(t, sin(t/20)*100, 20,20);
+// 	ellipse(t, -sin(t/20)*100, 20,20);
+
+// 	// fill(random(0,255),0,random(0,255));
+// 	// ellipse(t, cos(t/20)*100, 20,20);
+
+// }
+
+
+
+// // first experiment
+// void draw(){
+
+// 	// moving my origin canvas position 
+// 	translate(0, height/2);   // move the coordinate to left middle
+
+// 	ellipse(frameCount, sin(radians(frameCount))*200, 50, 50);
+// 	ellipse(frameCount, -sin(radians(frameCount))*200, 50, 50);
+
+// }
 
 
 // cos() starts at 1
